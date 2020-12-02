65d4a079390316cb6a8f07403ca66c2ad1b2469c
silvastephen <73506310+silvastephen@users.noreply.github.com>
2020-12-02 15:35:44 -0600

    setting up

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  hol up
   2. Which of the two uses a closure? How can you tell?
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
